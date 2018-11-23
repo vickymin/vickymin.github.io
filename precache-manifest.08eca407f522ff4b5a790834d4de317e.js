2212e4be76dc4046f89c5db671d9c523430531bf
vickymin <minjiepei@gmail.com>
2018-11-23 16:27:02 +0800

    Updates

---
+++ precache-manifest.08eca407f522ff4b5a790834d4de317e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d59ddebdea43ee7cf4c2",
+    "url": "/static/js/main.d59ddebd.chunk.js"
+  },
+  {
+    "revision": "db268a065d6295c84e8c",
+    "url": "/static/js/1.db268a06.chunk.js"
+  },
+  {
+    "revision": "d59ddebdea43ee7cf4c2",
+    "url": "/static/css/main.0fcca9e4.chunk.css"
+  },
+  {
+    "revision": "db268a065d6295c84e8c",
+    "url": "/static/css/1.53c36a49.chunk.css"
+  },
+  {
+    "revision": "1b3e43f7ab478ad00c234f456efe61c6",
+    "url": "/index.html"
+  }
+];
